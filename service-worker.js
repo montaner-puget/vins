432db1c38b9e45c9f465bd5d5d646ab0d3fa56f0
montaner-puget <leslie@ixtenso.fr>
2017-03-16 21:19:49 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 var CACHE_NAME = 'montaner-dependencies-cache';
 // Files required to make this app work offline
-var REQUIRED_FILES = ["https://lesliecordier.github.io/mtnr/", 
+var REQUIRED_FILES = [
     "https://lesliecordier.github.io/mtnr/css/jquery.mobile-1.4.5.css", 
     "https://lesliecordier.github.io/mtnr/css/style.css", 
     "https://lesliecordier.github.io/mtnr/js/jquery.js", 
@@ -18,12 +18,11 @@ var REQUIRED_FILES = ["https://lesliecordier.github.io/mtnr/",
     "https://lesliecordier.github.io/mtnr/images/verres-vins.jpg", 
     "https://lesliecordier.github.io/mtnr/images/sprites.gif", 
     "https://lesliecordier.github.io/mtnr/images/cepage.jpg", 
-    "https://fonts.gstatic.com/s/gentiumbookbasic/v7/IRFxB2matTxrjZt6a3FUnjHxYFfSxJ-QC83SOBxk38Y.woff2", 
-    "https://lesliecordier.github.io/mtnr/css/images/ajax-loader.gif", 
     "https://lesliecordier.github.io/mtnr/images/fond-accueil.jpg", 
     "https://lesliecordier.github.io/mtnr/icones/doc.png", 
     "https://lesliecordier.github.io/mtnr/icones/desc.gif", 
-    "https://lesliecordier.github.io/mtnr/icones/bg.gif"]
+    "https://lesliecordier.github.io/mtnr/icones/bg.gif"
+]
 
 self.addEventListener('install', function (event) {
     console.log('[install]');
